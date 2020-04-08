6d04019a5801ed8eda8cedfeb9f92f862d685edb
Agustin <tincho082009@gmail.com>
2020-04-08 16:41:13 -0300

    Funciona

--- app/src/main/java/com/example/practiconro3/MainActivity.java
+++ app/src/main/java/com/example/practiconro3/MainActivity.java
@@ -3,6 +3,8 @@ package com.example.practiconro3;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 import java.util.ArrayList;
 
@@ -14,10 +16,22 @@ public class MainActivity extends AppCompatActivity {
         protected void onCreate(Bundle savedInstanceState) {
                 super.onCreate(savedInstanceState);
                 setContentView(R.layout.activity_main);
+                cargarDatos();
+                generarListView();
+        }
+
+        public void generarListView(){
+                ArrayAdapter<Inmueble> adapter = new ListAdapter(this, R.layout.item, lista, getLayoutInflater());
+                ListView lv = findViewById(R.id.miLista);
+                lv.setAdapter(adapter);
+
+
         }
 
         public void cargarDatos(){
                 lista.add(new Inmueble(R.drawable.casa1, "Juana Koslay", 80000));
+                lista.add(new Inmueble(R.drawable.casa2, "Potrero de los Funes", 50000));
+                lista.add(new Inmueble(R.drawable.casa3, "El Trapiche", 90000));
         }
 }
 
